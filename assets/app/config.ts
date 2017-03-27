89e1b024e7a575817dd354c5e0d43a45e9b8cfe0
guardino <cesare.guardino@yahoo.com>
2017-03-27 12:42:30 +0100

    URL/UAA service updates

--- assets/app/config.ts
+++ assets/app/config.ts
@@ -1,9 +1,9 @@
-process.env.baseUrl = 'http://localhost:8080';
-// process.env.baseUrl = 'https://jdbc-spring-music.run.aws-usw02-pr.ice.predix.io';
+// process.env.baseUrl = 'http://localhost:8080';
+process.env.baseUrl = 'https://periodic-elements-demo-spring.run.aws-usw02-pr.ice.predix.io';
 
-process.env.appUrl = 'http://localhost:3000';
-// process.env.appUrl = 'https://material-database-uaa.run.aws-usw02-pr.ice.predix.io';
+// process.env.appUrl = 'http://localhost:3000';
+process.env.appUrl = 'https://periodic-elements-demo.run.aws-usw02-pr.ice.predix.io';
 
-process.env.uaaUrl = 'https://2f2bd91e-3b1b-4e16-9838-7f697b13c47e.predix-uaa.run.aws-usw02-pr.ice.predix.io';
-process.env.client_id = "foo";
-process.env.client_secret = "foo";
+process.env.uaaUrl = 'https://7391cf8a-2d32-45f5-8884-cf36e36c3fdb.predix-uaa.run.aws-usw02-pr.ice.predix.io';
+process.env.client_id = "elements";
+process.env.client_secret = "mendeleev";
