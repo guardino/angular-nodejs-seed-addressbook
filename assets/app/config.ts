1681553aa8c13926d1f53ea3622af411b9710c1e
guardino <cesare.guardino@yahoo.com>
2017-04-10 12:24:32 +0100

    Merge with master

--- assets/app/config.ts
+++ assets/app/config.ts
@@ -5,5 +5,5 @@ process.env.baseUrl = 'https://periodic-elements-demo-spring.run.aws-usw02-pr.ic
 process.env.appUrl = 'https://periodic-elements-demo.run.aws-usw02-pr.ice.predix.io';
 
 process.env.uaaUrl = 'https://7391cf8a-2d32-45f5-8884-cf36e36c3fdb.predix-uaa.run.aws-usw02-pr.ice.predix.io';
-process.env.client_id = "elements";
-process.env.client_secret = "mendeleev";
+process.env.client_id = "ZWxlbWVudHM=";
+process.env.client_secret = "bWVuZGVsZWV2";
